07e748569e6fc88ce206dd75778e9bc905d5b28f
James White <dev.jameswhite@gmail.com>
2026-06-10 10:54:18 +0100

    Add .dotenv function

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -14,3 +14,13 @@ function ql
     qlmanage -p $argv
 end
 
+function dotenv
+    if test (count $argv) -eq 0
+        echo "Error: dotenv requires a path argument" >&2
+        return 1
+    end
+    for line in (cat $argv | grep -v '^#')
+        set item (string split -m 1 '=' $line)
+        set -gx $item[1] $item[2]
+    end
+end
